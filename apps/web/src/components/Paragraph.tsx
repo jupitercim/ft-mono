07b3e5883e7328b1316afeab4ea562fc75a76669
kent.z <kent.z@binance.com>
2024-07-01 15:44:21 +0800

    feat: home mobile

---
+++ apps/web/src/components/Paragraph.tsx
@@ -0,0 +1,26 @@
+import { Box, Typography } from '@mui/material';
+import { makeStyles } from 'tss-react/mui';
+import { ReactNode } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+
+interface Props {
+  ns?: string | string[];
+  iKey: string;
+}
+
+export const Paragraph = ({ ns, iKey }: Props) => {
+  const { classes, cx } = useStyles();
+  const { t } = useTranslation(ns);
+
+  return (
+    <Trans
+      t={t}
+      i18nKey={iKey}
+      components={{
+        br: <br />,
+      }}
+    />
+  );
+};
+
+const useStyles = makeStyles()(theme => ({}));
