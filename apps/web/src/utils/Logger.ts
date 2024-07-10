7975c8dc95c832f47beecf79fdb915619c5e1b86
ramroll.w <ramroll.w@binance.com>
2024-07-10 18:24:53 +0800

    fix args

--- apps/web/src/utils/Logger.ts
+++ apps/web/src/utils/Logger.ts
@@ -2,11 +2,15 @@ export class Logger {
   private constructor(private name: string) {}
 
   public debug(...args: any[]) {
-    console.debug(`%c[${this.name}]`, 'color: blue;font-weight: bold;', ...args)
+    // eslint-disable-next-line no-console
+    console.debug(
+      `%c[${this.name}]`,
+      'color: blue;font-weight: bold;',
+      ...args,
+    );
   }
 
-
   static get(name: string) {
-    return new Logger(name)
+    return new Logger(name);
   }
 }
